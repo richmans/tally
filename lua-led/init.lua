57b14ee1a8acc7663be6f788b5c8add415bc9b92
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2018-08-15 20:14:44 +0200

    fixed remote configuration

--- lua-led/init.lua
+++ lua-led/init.lua
@@ -21,7 +21,7 @@ function init()
 end
 
 function init_leds()
-  for led in leds do
+  for i,led in ipairs(leds) do
     gpio.mode(led, gpio.OUTPUT)
   end
   set_led_state(LED_BLUE)
@@ -34,12 +34,12 @@ function blink(temp_ledstate)
     if ledstate == old_ledstate then
       set_led_state(old_ledstate)
     end
-  end
+  end)
 end
 
 function set_led_state(new_led_state)
   ledstate = new_led_state
-  for led in leds do
+  for i,led in ipairs(leds) do
     if led == ledstate then
       gpio.write(led, gpio.HIGH)
     else
@@ -75,8 +75,10 @@ function udp_listen()
 end
 
 function parse_packet(s, data, port, ip)
+  print("Parsing packet")
   command = string.byte(data)
   if command == 1 then
+    print("Command 1")
     message = string.format(string.char(4) .. "%s,%d", node.chipid(), channel)
     s:send(port, ip, message)
   elseif command == 2 then
@@ -90,15 +92,21 @@ function parse_packet(s, data, port, ip)
       --print("LED off!")
     end
   elseif command == 3 then
-    if node.chipid() == string.sub(message,2,7)
-      channel = tonumber(string.sub(message, -1))
+    print("Command 3")
+    print(node.chipid())
+    print(tonumber(string.sub(data,2,7),16))
+    if node.chipid() == tonumber(string.sub(data,2,7),16) then
+      print("Setting channelid")
+      channel = tonumber(string.sub(data, -1))
+      write_config()
       blink(LED_BLUE)
     end
   end
 end
 
 function read_config()
-  dofile(config_file)
+  local conf, err = loadfile(config_file)
+  conf()
 end
 
 function write_config()
